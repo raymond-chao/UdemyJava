d9c1885aac25b135e67d43a6643b16c2394cee3e
raymond-chao <raymond_chao45@hotmail.com>
2026-03-31 17:41:45 +0200

    challenges added

--- src/LinkedListChallenge/Main.java
+++ src/LinkedListChallenge/Main.java
@@ -1,6 +1,8 @@
 package LinkedListChallenge;
 
 import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
 
     record Place(String name, int distance) {
@@ -12,25 +14,34 @@ public class Main {
     }
     public static void addPlace(LinkedList<Place> list , Place place) {
         if (list.contains(place)) {
-            System.out.println(place.name + " is already in the list");
+            System.out.println(place.name() + " is already in the list");
             return;
         }
         for (Place p : list) {
-            if (p.name.equalsIgnoreCase(place.name)) {
+            if (p.name().equalsIgnoreCase(place.name())) {
                 System.out.println(p.name + " is already in the list");
                 return;
             }
         }
-    int matchedIndex = 0;
-        for (var listPlace : list) {
-            if (place.distance() < listPlace.distance()) {
-                list.add(matchedIndex, place);
-                matchedIndex++;
+        for (int i = 0; i < list.size(); i++) {
+            if (place.distance() < list.get(i).distance()) {
+                list.add(i, place);
                 return;
             }
         }
 
         list.add(place);
+    }
+
+    private static void printMenu(){
+        System.out.println("""
+                (F)orward
+                (B)ackward
+                (L)ist places
+                (M)enu
+                (Q)uit
+                """);
+
     }
 
     public static void main(String[] args) {
@@ -47,5 +58,65 @@ public class Main {
 
         placesToVisit.addFirst(new Place("Sydney", 0));
         System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+
+        while (!quitLoop) {
+            if (!iterator.hasPrevious()) {
+                System.out.println("Originating: " + iterator.hasNext());
+                forward = true;
+            }
+            if (!iterator.hasNext()) {
+                System.out.println("Final: " + iterator.hasPrevious());
+                forward = false;
+            }
+
+            System.out.println("Enter value: ");
+            String menuItem = scanner.nextLine().toUpperCase().substring(0,1);
+
+            switch (menuItem) {
+                case "F":
+                    System.out.println("User wants to go forward");
+                    if (!forward) {
+                        forward = true;
+                        if (iterator.hasNext()) {
+                            iterator.next();
+                        }
+                    }
+                    if (iterator.hasNext()) {
+                        System.out.println(iterator.next());
+
+                    }
+                    break;
+                case "B":
+                    System.out.println("User wants to go back");
+                    if (forward) {
+                        forward = false;
+                        if (iterator.hasPrevious()) {
+                            iterator.previous();
+                        }
+                    }
+                    if (iterator.hasPrevious()) {
+                        System.out.println(iterator.previous());
+
+                    }
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                case "L":
+                    System.out.println(placesToVisit);
+                    break;
+
+                default: quitLoop = true;
+                break;
+            }
+        }
+
     }
 }
